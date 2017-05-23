f93e446159349df8857e443a61644aa6c35a214c
Kai Ruhnau <kai.ruhnau@target-sg.com>
2017-05-23 14:47:04 +0200

    Clean up unused sysfs attributes

--- drivers/target-systemelektronik/u100-attributes.c
+++ drivers/target-systemelektronik/u100-attributes.c
@@ -24,11 +24,9 @@
 #define FPGA_SYSTEM_BASE	0x000
 #define FPGA_ADC_BASE		0x100
 #define FPGA_FLASH_BASE		0x200
-#define FPGA_DAC_BASE		0x300
 #define FPGA_MUX_BASE		0x400
 #define FPGA_STREAM_BASE	0x500
 #define FPGA_AFE_BASE		0x600
-#define FPGA_AFE2_BASE		0x700
 
 #define FPGA_EXT_FREQ		(FPGA_SYSTEM_BASE + 0x00)
 #define FPGA_PLL_MULT		(FPGA_SYSTEM_BASE + 0x08)
@@ -44,9 +42,6 @@
 #define FPGA_FLASH_STATUS	(FPGA_FLASH_BASE + 0x08)
 #define FPGA_FLASH_DATA		(FPGA_FLASH_BASE + 0x0c)
 
-#define FPGA_DAC1		(FPGA_DAC_BASE + 0x00)
-#define FPGA_DAC2		(FPGA_DAC_BASE + 0x04)
-
 #define FPGA_MUX		(FPGA_MUX_BASE + 0x00)
 
 #define FPGA_TRIGGER		(FPGA_STREAM_BASE + 0x00)
@@ -55,11 +50,13 @@
 #define FPGA_SYNC		(FPGA_STREAM_BASE + 0x0c)
 #define FPGA_PAUSE		(FPGA_STREAM_BASE + 0x10)
 #define FPGA_RESOLUTION		(FPGA_STREAM_BASE + 0x14)
+#define FPGA_PREPAUSE		(FPGA_STREAM_BASE + 0x1c)
+#define FPGA_TRIGGER_PAUSE	(FPGA_STREAM_BASE + 0x2c)
 
 #define FPGA_AFE_COMMAND	(FPGA_AFE_BASE + 0x00)
 #define FPGA_AFE_ON		(FPGA_AFE_BASE + 0x04)
-#define FPGA_HV_ON		(FPGA_AFE_BASE + 0x08)
-#define FPGA_HV			(FPGA_AFE_BASE + 0x0c)
+#define FPGA_AFE_HV_ON		(FPGA_AFE_BASE + 0x08)
+#define FPGA_AFE_HV		(FPGA_AFE_BASE + 0x0c)
 #define FPGA_AFE_DAC		(FPGA_AFE_BASE + 0x10)
 //... FPGA_AFE_DAC7		(FPGA_AFE_BASE + 0x28)
 
@@ -117,12 +114,12 @@ static ssize_t hv_show(struct device *dev, struct device_attribute *attr, char *
 	u32 value;
 
 	bar_write(dev, 0, FPGA_AFE_COMMAND);
-	bar_write(dev, 0, FPGA_HV_ON);
-	value = bar_read(dev, FPGA_HV_ON);
+	bar_write(dev, 0, FPGA_AFE_HV_ON);
+	value = bar_read(dev, FPGA_AFE_HV_ON);
 	if (value != 0) // There is an actual HV set
 	{
-		bar_write(dev, 0, FPGA_HV);
-		value = bar_read(dev, FPGA_HV);
+		bar_write(dev, 0, FPGA_AFE_HV);
+		value = bar_read(dev, FPGA_AFE_HV);
 	}
 
 	return scnprintf(buf, PAGE_SIZE, "%u\n", value);
@@ -136,8 +133,8 @@ static ssize_t hv_store(struct device *dev, struct device_attribute *attr, const
 		return -EINVAL;
 
 	bar_write(dev, 1, FPGA_AFE_COMMAND);
-	bar_write(dev, !!value, FPGA_HV_ON);
-	bar_write(dev, value, FPGA_HV);
+	bar_write(dev, !!value, FPGA_AFE_HV_ON);
+	bar_write(dev, value, FPGA_AFE_HV);
 
 	return count;
 }
@@ -269,8 +266,6 @@ VALUE_RO(build_number, FPGA_BUILD_NUMBER, "%d");
 DEVICE_ATTR_RO(version);
 VALUE_RW(adc, FPGA_ADC_CONFIG, "0x%x");
 BIN_ATTR(firmware, S_IWUSR, NULL, firmware_store, 0);
-VALUE_RW(fpga_dac1, FPGA_DAC1, "%d");
-VALUE_RW(fpga_dac2, FPGA_DAC2, "%d");
 VALUE_RW(mux, FPGA_MUX, "0x%x");
 VALUE_RW(trigger, FPGA_TRIGGER, "%d");
 VALUE_RW(samples, FPGA_SAMPLES, "%d");
@@ -278,6 +273,8 @@ VALUE_RW(acq, FPGA_ACQ, "%d");
 VALUE_WO(sync, FPGA_SYNC);
 VALUE_RW(pause, FPGA_PAUSE, "%d");
 VALUE64_RO(resolution, FPGA_RESOLUTION);
+VALUE_RW(prepause, FPGA_PREPAUSE, "%d");
+VALUE_RW(trigger_pause, FPGA_TRIGGER_PAUSE, "%d");
 DEVICE_ATTR_RW(afe_on);
 DEVICE_ATTR_RW(hv);
 DEVICE_ATTR(dac1, S_IWUSR | S_IRUGO, dac_show, dac_store);
@@ -298,8 +295,6 @@ static struct attribute *fpga_attrs[] = {
 	&dev_attr_build_number.attr,
 	&dev_attr_version.attr,
 	&dev_attr_adc.attr,
-	&dev_attr_fpga_dac1.attr,
-	&dev_attr_fpga_dac2.attr,
 	&dev_attr_mux.attr,
 	&dev_attr_trigger.attr,
 	&dev_attr_samples.attr,
@@ -307,6 +302,8 @@ static struct attribute *fpga_attrs[] = {
 	&dev_attr_sync.attr,
 	&dev_attr_pause.attr,
 	&dev_attr_resolution.attr,
+	&dev_attr_prepause.attr,
+	&dev_attr_trigger_pause.attr,
 	&dev_attr_afe_on.attr,
 	&dev_attr_hv.attr,
 	&dev_attr_dac1.attr,
